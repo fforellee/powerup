af86323993084a286adbb69685dc922f22adc0f2
fforelle <xdollboyx@gmail.com>
2021-12-05 15:24:30 -0300

    alterando curar

--- src/mechanics/Card.java
+++ src/mechanics/Card.java
@@ -58,11 +58,11 @@ public class Card {
   public void Curar(Variables Global) {
     if (Global.knightQuantity >= 4) {
       Global.playerHealth += 5;
-      Global.knightQuantity -= 3;
-      Global.computeTotalDamage();
+      Global.knightQuantity -= 1;
     } else {
       System.out.println("\n[!] A carta não pôde ser usada, o número de cavaleiros é muito baixo.");
     }
+      Global.computeTotalDamage();
   }
 
   public void Recrutar(Variables Global) {
